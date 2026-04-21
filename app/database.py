245077e7ee9f2a9fde02650d71569d054edfc183
caisarioihsandev <iscaisario@gmail.com>
2026-04-21 08:55:39 +0700

    variabel

--- app/database.py
+++ app/database.py
@@ -1,13 +1,12 @@
 import os
 import pymysql
-from config import Config
 
 def get_db():
     return pymysql.connect(
-        host=os.getenv("DB_HOST", Config.MYSQL_HOST),
-        user=os.getenv("DB_USER", Config.MYSQL_USER),
-        password=os.getenv("DB_PASSWORD", Config.MYSQL_PASSWORD),
-        database=os.getenv("DB_NAME", Config.MYSQL_DB),
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
         port=int(os.getenv("DB_PORT", 3306)),
         cursorclass=pymysql.cursors.DictCursor
     )
